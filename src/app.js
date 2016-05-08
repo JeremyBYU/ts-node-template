30fc7bce08c39ad7ba44e08caac0675411978b33
Jeremy Castagno <JeremyBYU@gmail.com>
2016-05-08 16:16:50 -0500

    Added Typings and Express

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-import Person from './person' // Javascript E
-import Greeter from './greeter/greeter'
-const p1 = new Person('Javascript Class, no types')
+
+import * as express from 'express' // Watch Intellisense go to work in VS Code
+import Person from './person' // Javascript Class
+import Greeter from './greeter/greeter' // Typescript Class
+
+const p1 = new Person('Javascript ES6 Class, no types')
 const g1 = new Greeter('Typescript Class, has types')
 
-setTimeout(() => {
-  console.log(p1.sayName())
-  console.log(g1.greet())
-}, 10000)
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(p1.sayName() + '<br>' + g1.greet())
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
+
+
