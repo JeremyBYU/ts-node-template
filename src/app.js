7c92c3a7f122c8b0c2c6c27cf261d2f5a125c74b
Jeremy Castagno <JeremyBYU@gmail.com>
2016-05-07 21:10:49 -0500

    Update Git Ignore

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Person from './person' // Javascript E
 import Greeter from './greeter/greeter'
 const p1 = new Person('Javascript Class, no types')
-const g1 = new Greeter('Typescript Class, has types')
+const g1 = new Greeter('Typescript Class, has types');
 
 setTimeout(() => {
   console.log(p1.sayName())
